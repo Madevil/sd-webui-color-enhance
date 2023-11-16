aa45c98ac7abff939b5cc985c0de0dd3d12d9212
MMaker <mmaker@mmaker.moe>
2023-11-15 20:32:42 -0500

    ComfyUI: Add color blend

--- mmaker_color_enhance_core.py
+++ mmaker_color_enhance_core.py
@@ -10,5 +10,15 @@ def color_enhance(arr, strength: float = 1) -> Image.Image:
     lch[:, :, 1] *= 100/(lerp(100, lch[:, :, 1].max(), strength)) # Normalize chroma component
     return Image.fromarray(np.array(skimage.color.lab2rgb(lab=skimage.color.lch2lab(lch=lch)) * 255, dtype=np.uint8))
 
+def color_blend(arr1, arr2) -> Image.Image:
+    hsv1 = skimage.color.rgb2hsv(rgb=np.array(arr1, dtype=np.uint8))
+    hsv2 = skimage.color.rgb2hsv(rgb=np.array(arr2, dtype=np.uint8))
+
+    hsv1[..., 0] = hsv2[..., 0]
+    hsv1[..., 1] = hsv2[..., 1]
+
+    out = skimage.color.hsv2rgb(hsv1)
+    return Image.fromarray(np.array(out * 255, dtype=np.uint8))
+
 def lerp(a: float, b: float, t: float) -> float:
     return (1 - t) * a + t * b
